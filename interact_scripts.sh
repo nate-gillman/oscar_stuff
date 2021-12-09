df6cd308dbbee8950efa5e688b6dfab406b428b7
Nate Gillman <nategillman1@gmail.com>
2021-12-09 10:09:33 -0500

    Update interact_scripts.sh

--- interact_scripts.sh
+++ interact_scripts.sh
@@ -1,5 +1,5 @@
 # Requesting a 4 hour interactive GPU job with 1 core and 63gb of memory
 interact -q gpu -g 1 -t 04:00:00 -m 63g
 
-# Requesting a 1 hour interactive CPU job with 1 core and 31gb of memory
-interact -n 1 -t 01:00:00 -m 31g
+# Requesting an 8 hour interactive CPU job with 1 core and 31gb of memory
+interact -n 1 -t 08:00:00 -m 31g
