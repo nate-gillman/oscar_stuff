560d19759d1978223ad87768cd21d79f22500737
Nate Gillman <nategillman1@gmail.com>
2021-12-10 10:48:27 -0500

    Update file_management.sh

--- file_management.sh
+++ file_management.sh
@@ -1,2 +1,9 @@
 # For zipping all files in the current directory
 zip -r archive.zip .
+
+# For computing how much data is being used in each folder in the current directory; gives detailed breakdown
+module load ncdu
+ncdu .
+
+# For computing how much data is being used in each folder in the current directory; gives less detailed breakdown
+du -s -m .
