0b6d8c3ec0e6a02b1f9c1d95e281da227ecd2c28
Nate Gillman <nategillman1@gmail.com>
2022-01-18 13:16:39 -0500

    Add files via upload

---
+++ env_management.sh
@@ -0,0 +1,15 @@
+# For zipping all files in the current directory
+zip -r archive.zip .
+
+# For computing how much data is being used in each folder in the current directory; gives detailed breakdown
+module load ncdu
+ncdu .
+
+# For computing how much data is being used in each folder in the current directory; gives less detailed breakdown
+du -s -m .
+
+# For computing how much data is being used in the folder nates_stuff in the current directory
+du -sh nates_stuff/
+
+# For computing how much data is being used in the subdirectories of the folder nates_stuff in the current directory
+du -shc nates_stuff/*
