c4f5de2d6d44b5cb0dcaf674ff1f6fea73980e3f
Nate Gillman <nategillman1@gmail.com>
2022-01-24 14:56:40 -0500

    Add files via upload

---
+++ remote_IDE.sh
@@ -0,0 +1,4 @@
+# Sometimes my Remote IDE (sshing to Oscar through VSCode) doesn't work...
+# To fix it, I need to edit inside the following doc:
+vi ~/.ssh/known_hosts
+# and remove (dd) all lines beginning with "Oscar2", then save and exit (wq!)
