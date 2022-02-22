a2182c7e32b0364ed8c85ab8947b379aaa17f690
Nate Gillman <nategillman1@gmail.com>
2022-02-22 12:23:31 -0500

    Delete remote_IDE.sh

--- remote_IDE.sh
+++
@@ -1,4 +0,0 @@
-# Sometimes my Remote IDE (sshing to Oscar through VSCode) doesn't work...
-# To fix it, I need to edit inside the following doc on my local machine:
-vi ~/.ssh/known_hosts
-# and remove (dd) all lines beginning with "Oscar2", then save and exit (wq!)
